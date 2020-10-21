b5be7ba1e51ffcfec352cc0e948494d16ea6eb90
hwang jae hyeon <hjh450858@gmail.com>
2020-10-21 23:20:00 +0900

    프로토콜로 데이터 전달해보기

--- ExampleProtocol/ViewController.swift
+++ ExampleProtocol/ViewController.swift
@@ -9,11 +9,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imgView: UIImageView!
+    @IBOutlet weak var lblTitle: UILabel!
+    @IBOutlet weak var btnData: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        lblTitle.backgroundColor = .yellow
     }
-
-
+    
+    @IBAction func btnDataClicked(_ sender: UIButton) {
+        let newVC = storyboard?.instantiateViewController(withIdentifier: "DataViewController") as! DataViewController
+        // 'DataViewController'에 있는 dataDelegate 대리자를 'ViewController'에 적용
+        newVC.delegate = self
+        present(newVC, animated: true, completion: nil)
+    }
 }
 
+// 'DataViewController'에 있는 프로토콜 채택
+extension ViewController: SendDataDelegate {
+    func messageData(title: String) {
+        self.lblTitle.text = title
+        print("ViewController - \(title)")
+    }
+}
